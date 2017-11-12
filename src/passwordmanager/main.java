8988c9625151d3e1fd3d772b1258b715b75e235e
pmbonneau <pierremarc_999@hotmail.com>
2017-11-12 18:15:16 -0500

    Added JSON format parser.

--- src/passwordmanager/main.java
+++ src/passwordmanager/main.java
@@ -12,10 +12,16 @@ import commandLineArgsParser.HelpFormatter;
 import commandLineArgsParser.Option;
 import commandLineArgsParser.Options;
 import commandLineArgsParser.ParseException;
+import jsonParser.JSONObject;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
@@ -24,6 +30,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 import java.util.Scanner;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -31,6 +38,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import jdk.nashorn.internal.parser.JSONParser;
 import sun.misc.BASE64Encoder;
 
 /**
@@ -49,20 +57,28 @@ public class main {
         // Using Apache Commons CLI
         Options options = new Options();
 
+        Option optAddEntry = new Option("a", "add", true, "add an entry to password manager");
+        optAddEntry.setRequired(false);
+        options.addOption(optAddEntry);
+        
+        Option optReadEntries = new Option("l", "read", true, "read entries from password manager");
+        optReadEntries.setRequired(false);
+        options.addOption(optReadEntries);
+        
         Option optService = new Option("s", "service", true, "service which uses the password");
         optService.setRequired(false);
         options.addOption(optService);
         
         Option optAddress = new Option("u", "address", true, "service address");
-        optAddress.setRequired(true);
+        optAddress.setRequired(false);
         options.addOption(optAddress);
         
         Option optUsername = new Option("n", "username", true, "account username");
-        optUsername.setRequired(true);
+        optUsername.setRequired(false);
         options.addOption(optUsername);
         
         Option optPassword = new Option("p", "password", true, "account password");
-        optPassword.setRequired(true);
+        optPassword.setRequired(false);
         options.addOption(optPassword);
         
         Option optComment = new Option("c", "comment", true, "entry comment");
@@ -86,35 +102,97 @@ public class main {
             return;
         }
 
+        String BagPasswordWrite = cmd.getOptionValue("add");
+        String BagPasswordRead = cmd.getOptionValue("l");
         String Service = cmd.getOptionValue("service");
         String Address = cmd.getOptionValue("address");
         String Username = cmd.getOptionValue("username");
         String Password = cmd.getOptionValue("password");
         String Comment = cmd.getOptionValue("comment");
         
-        //File KeyBagFile = new File("keybag.txt");
-        //if (KeyBagFile.exists() == false)
-        //{
-           // Scanner inputReader = new Scanner(System.in);
+        File KeyBagFile = new File("keybag.txt");
+        if (KeyBagFile.exists() == false)
+        {
+            //Scanner inputReader = new Scanner(System.in);
             //System.out.println("Please enter new manager password.");
             //String ManagerPassword = inputReader.nextLine();
-            //createBag(ManagerPassword);
-       // }
+            createBag();
+        }
         
-        //String EncryptedBagPassword = 
-        //doEncryption(Username);
+        if (BagPasswordRead == null)
+        {
+            String EncryptedService = doEncryption(Service,BagPasswordWrite);
+            String EncryptedAddress = doEncryption(Address,BagPasswordWrite);
+            String EncryptedUsername = doEncryption(Username,BagPasswordWrite);
+            String EncryptedPass = doEncryption(Password,BagPasswordWrite);
+            String EncryptedComment = "";
+            if (Comment != null)
+            {
+                EncryptedComment = doEncryption(Comment,BagPasswordWrite);
+            }
+
+            FileWriter writefile = new FileWriter("keybag.txt", true);
+            FileReader readfile = new FileReader("keybag.txt");
+            Path Path = Paths.get("keybag.txt");
+            LineNumberReader LineCount = new LineNumberReader(readfile);
+            String ActualLineCount = Long.toString(Files.lines(Path).count());
+            JSONObject obj = new JSONObject();
+            obj.put("Line", ActualLineCount);
+            obj.put("Service", EncryptedService);
+            obj.put("URL", EncryptedAddress);
+            obj.put("Name", EncryptedUsername);
+            obj.put("Password", EncryptedPass);
+            obj.put("Comment", EncryptedComment);
+            writefile.write(obj.toString());
+            writefile.write("\r\n");
+            //writefile.write(ActualLineCount + "|$|" + EncryptedService + "|$|" + EncryptedAddress + "|$|" + EncryptedUsername + "|$|" + EncryptedPass + "|$|" + EncryptedComment);
+            writefile.close(); 
+        }
+        
+        if (BagPasswordWrite == null)
+        {
+           Path Path = Paths.get("keybag.txt");
+           List<String> lines = Files.readAllLines(Path);
+           for (int i = 0; i < lines.size(); i++)
+           {
+                JSONObject obj = new JSONObject(lines.get(i));
+                System.out.println(obj.get("Line"));
+                String DecryptedService = doDecryption(obj.get("Service").toString(),BagPasswordRead);
+                System.out.println(DecryptedService);
+                String DecryptedAddress = doDecryption(obj.get("URL").toString(),BagPasswordRead);
+                System.out.println(DecryptedAddress);
+                //String DecryptedUsername = doDecryption(obj.get("Name").toString(),BagPasswordRead);
+                String DecryptedUsername = "*****";
+                System.out.println(DecryptedUsername);
+                //String DecryptedPassword = doDecryption(obj.get("Password").toString(),BagPasswordRead);
+                String DecryptedPassword = "*****";
+                System.out.println(DecryptedPassword);
+                if (Comment == null)
+                {
+                    String DecryptedComment = ""; 
+                    System.out.println(DecryptedComment);
+                }
+                else
+                {
+                    String DecryptedComment = doDecryption(obj.get("Comment").toString(),BagPasswordRead);
+                    System.out.println(DecryptedComment);
+                }
+           }
+        }
+        
         
     }
     
-    public static void createBag(String BagPassword) throws IOException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    public static void createBag() throws IOException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
     {
-        //String EncryptedBagPassword = doEncryption(BagPassword);
         FileWriter writefile = new FileWriter("keybag.txt", true);
-        
-    //    writefile.write(IVData);
-        writefile.write("\n");
-   //     writefile.write(KeyData);
+        //writefile.write("Line|$|Service|$|URL|$|Name|$|Password|$|Comment");
         writefile.close();
+    }
+    
+    public static String[] LineParser(String LineToParse)
+    {
+        return LineToParse.split("|$|");
     }
     
     public static String doEncryption(String plainText, String key) throws Exception 
